9752c8b074c842cd5b69226595155088b373bf00
Corey Hayden <cghayden@gmail.com>
2023-12-11 12:45:01 -0500

    added conform, and a traditional reaact upload handler boilerplate

--- app/routes/parent.dancer.$id.tsx
+++ app/routes/parent.dancer.$id.tsx
@@ -3,6 +3,15 @@ import { Form, useLoaderData } from '@remix-run/react'
 import { ContentContainer } from '~/components/styledComponents/ContentContainer'
 import { getDancer } from '~/models/dancer.server'
 
+import { useForm } from '@conform-to/react'
+import { parse } from '@conform-to/zod'
+import { z } from 'zod'
+import { useState } from 'react'
+
+const schema = z.object({
+  imgFile: z.instanceof(File, { message: 'file is required' }),
+})
+
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const dancerId = params.id
   console.log('dancerId', dancerId)
@@ -18,6 +27,31 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 
 export default function DancerIndex() {
   const { dancer } = useLoaderData<typeof loader>()
+  const [file, setFile] = useState(undefined)
+  console.log('file', file)
+
+  const [form, { imgFile }] = useForm({
+    onValidate({ formData }) {
+      return parse(formData, { schema })
+    },
+  })
+  console.log('imgFile from component', imgFile)
+
+  const onFileChange = (e) => {
+    console.log('file input change', e.target.files)
+    setFile(e.target.files[0])
+  }
+
+  const handleImageSave = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    console.log('e:', e)
+    const target = e.target as typeof e.target & {
+      imgFile: { value: string }
+    }
+    const imgFile = target.imgFile.value // typechecks!
+    console.log('imgFile in handle submit', imgFile)
+    // etc...
+  }
 
   return (
     <>
@@ -29,12 +63,22 @@ export default function DancerIndex() {
         {dancer.img ? (
           <p>display image</p>
         ) : (
-          <Form>
+          <Form
+            encType='multipart/form-data'
+            {...form.props}
+            onSubmit={(e) => handleImageSave(e)}
+          >
             <div className='input_item'>
-              <div>
-                <label>Add an image for {dancer.firstName}</label>
-                <input type='file' accept='image/*' name='imgFile' />
-              </div>
+              <label className='block'>
+                Add an image for {dancer.firstName}
+              </label>
+              <input
+                type='file'
+                accept='image/*'
+                name='imgFile'
+                onChange={(e) => onFileChange(e)}
+              />
+              <button>Save Image</button>
             </div>
           </Form>
         )}
